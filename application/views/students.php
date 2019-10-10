8f98e338e696501a05f8bebf6a61249f334c8e9e
SharayuFaye <sharayu@servicenet.in>
2019-10-10 12:36:42 +0530

    scroll fix

--- application/views/students.php
+++ application/views/students.php
@@ -75,122 +75,154 @@
 					<input type="submit" id="upload" name="upload_xls" class="btn btn-primary" value="Upload XLS"> 
 			</div> 
                 <!--  End for upload -->  
-		</div> 
-							<table class="table table-bordered table-striped mb-0" id="datatable-tabletools">
-									<thead>
-										<tr>
-  											<th></th>  
-											<th>Class</th>
-											<th>Sections</th> 
-											<th>Student Name</th> 
-											<th>Student DOB</th> 
-											<th>Student Adhar No.</th> 
-											<th>Profile</th>
-											<th>Roll No</th>  
-											<th>Batch</th> 
-											<th>Father MailId/Username</th>  
-											<th>Parent Name</th>
-											<th>Parent Mob No</th>
-											<th>Mother Name</th>
-											<th>Mother MailID</th>
-											<th>Mother Mob No</th>
-											<th>Parent Scan Id</th> 
-											<th>Bus</th>
-											<th>Pickup Point</th>
-											<th>Join Date</th>
-											<th>Actions</th>
-										</tr>
-									</thead>
-									<tbody> 
-										<?php $i=1; foreach ($students_show as $row) { ?>
-										<tr data-item-id="1">
-										    <td><input type="checkbox" name="del_all[]"  id="del_all" value="<?php echo $row->id;?>"></td> 
-											<td><?php echo $row->class;?></td>  
-											<td><?php echo $row->sections;?></td>  
-											<td><?php echo $row->student_name;?></td>  
-											<td><?php echo $row->dob;?></td>  
-											<td><?php echo $row->adhar;?></td>  
-											<td><img src="<?php echo base_url(); ?>profile/<?php echo $row->profile;?>" width="35" height="35"/></td>  
-											<td><?php echo $row->roll_number;?></td>  
-											<td><?php echo $row->batch;?></td>  
-											<td><?php echo $row->username;?></td>   
-											<td><?php echo $row->parent_name;?></td>  
-											<td><?php echo $row->parent_mob;?></td> 
-											<td><?php echo $row->mother_name;?></td> 
-											<td><?php echo $row->mother_mail;?></td> 
-											<td><?php echo $row->mother_mob;?></td>  
-											<td><?php echo $row->parent_scan_id;?></td>
-											<td><?php echo $row->bus_number;?></td>  
-											<td><?php echo $row->pickup_point;?></td>  
-											<td><?php echo $row->join_date;?></td>  
- 
-											<td class="actions">
-												<a href="#" class="hidden on-editing save-row"><i class="fa fa-save"></i></a>
-												<a href="#" class="hidden on-editing cancel-row"><i class="fa fa-times"></i></a>
-												<a href="#" class="on-default edit-row"><i class="fa fa-pencil" 
-												onclick="edit('<?php echo $row->id;?>','<?php echo $row->users_id;?>' ,
-												'<?php echo $row->class_id;?>' ,'<?php echo $row->sections;?>','<?php echo $row->sections_id;?>' ,
-												'<?php echo $row->student_name;?>','<?php echo $row->dob;?>' , 
-												'<?php echo $row->adhar;?>' ,'<?php echo $row->profile;?>',  
-												'<?php echo $row->roll_number;?>' ,'<?php echo $row->batch;?>' ,
-												'<?php echo $row->username;?>' ,
-												'<?php echo $row->password;?>','<?php echo $row->parent_name;?>',
-    											'<?php echo $row->parent_mob;?>','<?php echo $row->mother_name;?>', 
-    											'<?php echo $row->mother_mail;?>', '<?php echo $row->mother_mob;?>',
-    											'<?php echo $row->parent_scan_id;?>',
-    											'<?php echo $row->bus_id;?>' ,'<?php echo $row->bus_number;?>' ,
-												'<?php echo $row->route_id;?>' ,'<?php echo $row->route_name;?>' ,
-												'<?php echo $row->trans_id;?>' ,'<?php echo $row->pickup_point;?>' ,
-												'<?php echo $row->join_date;?>' )"></i></a>
-												<a href="#" class="on-default remove-row"><i class="fa fa-trash-o" onclick="del('<?php echo $row->id;?>')"></i></a>
-											</td>
-										</tr> 
-										<?php $i++;} ?> 
-									  
-									</tbody>
-								</table>
-					 <?php echo form_close(); ?>
-							</div>
-						</section>
+		</div>  
+			<table class="table table-bordered table-striped mb-0" id="datatable-tabletools">
+					<thead>
+						<tr>
+							<th></th>  
+							<th>Class</th>
+							<th>Sections</th> 
+							<th>Student Name</th> 
+							<th>Student DOB</th> 
+							<th>Student Adhar No.</th> 
+							<th>Profile</th>
+							<th>Roll No</th>  
+							<th>Batch</th> 
+							<th>Father MailId/Username</th>  
+							<th>Parent Name</th>
+							<th>Parent Mob No</th>
+							<th>Mother Name</th>
+							<th>Mother MailID</th>
+							<th>Mother Mob No</th>
+							<th>Parent Scan Id</th> 
+							<th>Bus</th>
+							<th>Pickup Point</th>
+							<th>Join Date</th>
+							<th>Actions</th>
+						</tr>
+					</thead>
+					<tbody> 
+						<?php $i=1; foreach ($students_show as $row) { ?>
+						<tr data-item-id="1">
+						    <td><input type="checkbox" name="del_all[]"   value="<?php echo $row->id;?>"></td> 
+							<td><?php echo $row->class;?></td>  
+							<td><?php echo $row->sections;?></td>  
+							<td><?php echo $row->student_name;?></td>  
+							<td><?php echo $row->dob;?></td>  
+							<td><?php echo $row->adhar;?></td>  
+							<td><img src="<?php echo base_url(); ?>profile/<?php echo $row->profile;?>" width="35" height="35"/></td>  
+							<td><?php echo $row->roll_number;?></td>  
+							<td><?php echo $row->batch;?></td>  
+							<td><?php echo $row->username;?></td>   
+							<td><?php echo $row->parent_name;?></td>  
+							<td><?php echo $row->parent_mob;?></td> 
+							<td><?php echo $row->mother_name;?></td> 
+							<td><?php echo $row->mother_mail;?></td> 
+							<td><?php echo $row->mother_mob;?></td>  
+							<td><?php echo $row->parent_scan_id;?></td>
+							<td><?php echo $row->bus_number;?></td>  
+							<td><?php echo $row->pickup_point;?></td>  
+							<td><?php echo $row->join_date;?></td>  
+							<td class="actions">
+								<a href="#" class="hidden on-editing save-row"><i class="fa fa-save"></i></a>
+								<a href="#" class="hidden on-editing cancel-row"><i class="fa fa-times"></i></a>
+								<a href="#" class="on-default edit-row"><i class="fa fa-pencil" 
+								onclick="edit('<?php echo $row->id;?>','<?php echo $row->users_id;?>' ,
+								'<?php echo $row->class_id;?>' ,'<?php echo $row->sections;?>','<?php echo $row->sections_id;?>' ,
+								'<?php echo $row->student_name;?>','<?php echo $row->dob;?>' , 
+								'<?php echo $row->adhar;?>' ,'<?php echo $row->profile;?>',  
+								'<?php echo $row->roll_number;?>' ,'<?php echo $row->batch;?>' ,
+								'<?php echo $row->username;?>' ,
+								'<?php echo $row->password;?>','<?php echo $row->parent_name;?>',
+								'<?php echo $row->parent_mob;?>','<?php echo $row->mother_name;?>', 
+								'<?php echo $row->mother_mail;?>', '<?php echo $row->mother_mob;?>',
+								'<?php echo $row->parent_scan_id;?>',
+								'<?php echo $row->bus_id;?>' ,'<?php echo $row->bus_number;?>' ,
+								'<?php echo $row->route_id;?>' ,'<?php echo $row->route_name;?>' ,
+								'<?php echo $row->trans_id;?>' ,'<?php echo $row->pickup_point;?>' ,
+								'<?php echo $row->join_date;?>' )"></i></a>
+								<a href="#" class="on-default remove-row"><i class="fa fa-trash-o" onclick="del('<?php echo $row->id;?>')"></i></a>
+							</td>
+						</tr> 
+						<?php $i++;} ?> 
+					  
+					</tbody>
+				</table>
+			</div>
+	 <?php echo form_close(); ?>
+		</section>
 					<!-- end: page -->
 				</section>
 			<?php include('include/footer.php');?>			
+		
+		<!-- Vendor -->
+		<script src="<?php echo base_url(); ?>vendor/jquery/jquery.js"></script>
+		<script src="<?php echo base_url(); ?>vendor/jquery-browser-mobile/jquery.browser.mobile.js"></script>
+		<script src="<?php echo base_url(); ?>vendor/popper/umd/popper.min.js"></script>
+		<script src="<?php echo base_url(); ?>vendor/bootstrap/js/bootstrap.js"></script>
+		<script src="<?php echo base_url(); ?>vendor/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+		<script src="<?php echo base_url(); ?>vendor/common/common.js"></script>
+		<script src="<?php echo base_url(); ?>vendor/nanoscroller/nanoscroller.js"></script>
+		<script src="<?php echo base_url(); ?>vendor/magnific-popup/jquery.magnific-popup.js"></script>
+		<script src="<?php echo base_url(); ?>vendor/jquery-placeholder/jquery-placeholder.js"></script> 
+		<!-- Specific Page Vendor -->
+		<script src="<?php echo base_url(); ?>vendor/select2/js/select2.js"></script>
+		<script src="<?php echo base_url(); ?>vendor/datatables/media/js/jquery.dataTables.min.js"></script>
+		<script src="<?php echo base_url(); ?>vendor/datatables/media/js/dataTables.bootstrap4.min.js"></script>
+		<script src="<?php echo base_url(); ?>vendor/datatables/extras/TableTools/Buttons-1.4.2/js/dataTables.buttons.min.js"></script>
+		<script src="<?php echo base_url(); ?>vendor/datatables/extras/TableTools/Buttons-1.4.2/js/buttons.bootstrap4.min.js"></script>
+		<script src="<?php echo base_url(); ?>vendor/datatables/extras/TableTools/Buttons-1.4.2/js/buttons.html5.min.js"></script>
+		<script src="<?php echo base_url(); ?>vendor/datatables/extras/TableTools/Buttons-1.4.2/js/buttons.print.min.js"></script> 
+		<script src="<?php echo base_url(); ?>vendor/datatables/extras/TableTools/JSZip-2.5.0//jszip.min.js"></script>
+		<script src="<?php echo base_url(); ?>vendor/datatables/extras/TableTools/pdfmake-0.1.32/pdfmake.min.js"></script>
+		<script src="<?php echo base_url(); ?>vendor/datatables/extras/TableTools/pdfmake-0.1.32/vfs_fonts.js"></script> 
+		<!-- Theme Base, Components and Settings -->
+		<script src="<?php echo base_url(); ?>js/theme.js"></script> 
+		<!-- Theme Custom -->
+		<script src="<?php echo base_url(); ?>js/custom.js"></script> 
+		<!-- Theme Initialization Files -->
+		<script src="<?php echo base_url(); ?>js/theme.init.js"></script> 
+		<!-- Examples<?php echo base_url(); ?> -->
+		<script src="<?php echo base_url(); ?>js/examples/examples.datatables.default.js"></script>
+		<script src="<?php echo base_url(); ?>js/examples/examples.datatables.row.with.details.js"></script>
+		<script src="<?php echo base_url(); ?>js/examples/examples.datatables.tabletools.js"></script>
 
-<!-- Vendor -->
-<script src="<?php echo base_url(); ?>vendor/jquery/jquery.js"></script>
-<script src="<?php echo base_url(); ?>vendor/jquery-browser-mobile/jquery.browser.mobile.js"></script>
-<script src="<?php echo base_url(); ?>vendor/popper/umd/popper.min.js"></script>
-<script src="<?php echo base_url(); ?>vendor/bootstrap/js/bootstrap.js"></script>
-<script src="<?php echo base_url(); ?>vendor/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
-<script src="<?php echo base_url(); ?>vendor/common/common.js"></script>
-<script src="<?php echo base_url(); ?>vendor/nanoscroller/nanoscroller.js"></script>
-<script src="<?php echo base_url(); ?>vendor/magnific-popup/jquery.magnific-popup.js"></script>
-<script src="<?php echo base_url(); ?>vendor/jquery-placeholder/jquery-placeholder.js"></script> 
-<!-- Specific Page Vendor -->
-<script src="<?php echo base_url(); ?>vendor/select2/js/select2.js"></script>
-<script src="<?php echo base_url(); ?>vendor/datatables/media/js/jquery.dataTables.min.js"></script>
-<script src="<?php echo base_url(); ?>vendor/datatables/media/js/dataTables.bootstrap4.min.js"></script>
-<script src="<?php echo base_url(); ?>vendor/datatables/extras/TableTools/Buttons-1.4.2/js/dataTables.buttons.min.js"></script>
-<script src="<?php echo base_url(); ?>vendor/datatables/extras/TableTools/Buttons-1.4.2/js/buttons.bootstrap4.min.js"></script>
-<script src="<?php echo base_url(); ?>vendor/datatables/extras/TableTools/Buttons-1.4.2/js/buttons.html5.min.js"></script>
-<script src="<?php echo base_url(); ?>vendor/datatables/extras/TableTools/Buttons-1.4.2/js/buttons.print.min.js"></script> 
-<script src="<?php echo base_url(); ?>vendor/datatables/extras/TableTools/JSZip-2.5.0//jszip.min.js"></script>
-<script src="vendor/datatables/extras/TableTools/pdfmake-0.1.32/pdfmake.min.js"></script>
-<script src="<?php echo base_url(); ?>vendor/datatables/extras/TableTools/pdfmake-0.1.32/vfs_fonts.js"></script> 
-<!-- Theme Base, Components and Settings -->
-<script src="<?php echo base_url(); ?>js/theme.js"></script> 
-<!-- Theme Custom -->
-<script src="<?php echo base_url(); ?>js/custom.js"></script> 
-<!-- Theme Initialization Files -->
-<script src="<?php echo base_url(); ?>js/theme.init.js"></script> 
-<!-- Examples<?php echo base_url(); ?> -->
-<script src="js/examples/examples.datatables.default.js"></script>
-<script src="<?php echo base_url(); ?>js/examples/examples.datatables.row.with.details.js"></script>
-<script src="<?php echo base_url(); ?>js/examples/examples.datatables.tabletools.js"></script>
-	
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script>
 <script type="text/javascript">
+
+$(document).ready(function() {
+     $('#datatable-tabletools').DataTable( {
+    		destroy: true,
+            dom: 'Bfrtip',  
+            scrollX : true,
+            scrollCollapse : true,
+        	 buttons : [
+                 {
+                	 extend: 'excel',
+                     title: 'Students',
+                    footer: true,
+                  	 exportOptions: {
+                        columns: [1,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17]
+                    }
+                 },
+                 {
+                	 extend: 'print',
+                     title: 'Students',
+                 	  footer: true,
+                 	  exportOptions: {
+                        columns: [1,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17]
+                    }
+                 },
+                 { 
+                     	extend: 'pdf', 
+                        title: 'Students',
+                   	    exportOptions: {
+                        columns: [1,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17]
+                  } 
+               }
+       		]
+  } ); 
+} );
+
 
 $('button[id="delToTable"]').on('click', function(e) {
 	var checkedNum = $('input[name="del_all[]"]:checked').length;
@@ -1128,7 +1160,7 @@ function validateImageE(id) {
 					 <div class="modal-text">
 	                 <p>Are you sure that you want to delete this row, this will delete all data of student?</p>
 	                 <input type="hidden" id="id_del" name="id" class="form-control">
-						<input type="hidden" id="student_name" name="role" class="form-control">
+						 
                     </div>
 				 </div>
 				</div>
