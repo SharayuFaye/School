4ab8eb5e5fd001f5ec873570560f4fda6484ac40
SharayuFaye <sharayu@servicenet.in>
2019-12-04 14:57:47 +0530

    query update

--- application/models/M_notifications.php
+++ application/models/M_notifications.php
@@ -123,9 +123,8 @@ class m_notifications extends CI_Model {
 		if($class_id != null){
         	
         	$this->db->select('n.message,n.title,n.datetime,n.school_id')
-            	->from('notification n') 
-            	->where(array('n.roles_id' => 'student'))
-            	->or_where('n.roles_id = "all"', NULL, FALSE)
+            	->from('notification n')  
+            	->where('(n.roles_id = "student" or n.roles_id = "all")', NULL, FALSE)
             	->where(array( 'n.school_id' => $school_id))
             	->where('(n.sections_id =' . $sections_id . ' or n.sections_id IS NULL or n.sections_id = 0)',NULL, FALSE)
             	->where('(n.class_id =' . $class_id . ' or n.class_id = 0)', NULL, FALSE)
@@ -158,7 +157,7 @@ class m_notifications extends CI_Model {
         $this->db->join('school s', 'u.school_id=s.id', 'left');
         $this->db->where(array( 'u.token' =>$token));
         $this->db->where(array( 'u.id' =>$user_id));
-        $this->db->or_where('n.roles_id = "all"', NULL, FALSE);
+        $this->db->where('(n.roles_id = u.role or n.roles_id = "all")', NULL, FALSE);
         $this->db->order_by('n.datetime','asc');
         
         $query = $this->db->get();
