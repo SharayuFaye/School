8dea59158517ddfc332f35aaa1508d29aa8ca82c
SharayuFaye <sharayu@servicenet.in>
2019-12-05 19:01:24 +0530

    change quites

--- application/models/M_notifications.php
+++ application/models/M_notifications.php
@@ -141,7 +141,7 @@ class m_notifications extends CI_Model {
         
         
         
-        $query = $this->db->query( ' SELECT  n.* FROM   notification n JOIN   users u ON     n.roles_id = u.role LEFT JOIN tokens t ON t.user_id = u.id WHERE n.school_id = '.$school_id.' AND t.token = '.$user_id.' UNION SELECT n.* FROM    notification n WHERE  n.roles_id = "all"');
+        $query = $this->db->query( ' SELECT  n.* FROM   notification n JOIN   users u ON     n.roles_id = u.role LEFT JOIN tokens t ON t.user_id = u.id WHERE n.school_id = '.$school_id.' AND t.token = "'.$user_id.'" UNION SELECT n.* FROM    notification n WHERE  n.roles_id = "all"');
        	log_message('debug',$this->db->last_query()); 
         if($query)
         {
