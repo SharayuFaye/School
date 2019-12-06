e7f373666a89e8b1120c8d6ab619512299ec99e1
SharayuFaye <sharayu@servicenet.in>
2019-12-06 10:57:41 +0530

    heck for query issue merge

--- application/models/M_notifications.php
+++ application/models/M_notifications.php
@@ -140,11 +140,11 @@ class m_notifications extends CI_Model {
         $this->db->order_by('n.datetime','desc');
         
         
-        $query = $this->db->query('SELECT n.* FROM notification n JOIN users u ON n.roles_id = u.role LEFT JOIN tokens t ON t.user_id = u.id WHERE n.school_id = '.$school_id.' AND t.token = "'.$user_id.'" UNION SELECT n.* FROM notification n WHERE n.roles_id = "all" and n.school_id = '.$school_id);
+        $query1 = $this->db->query('SELECT n.* FROM notification n JOIN users u ON n.roles_id = u.role LEFT JOIN tokens t ON t.user_id = u.id WHERE n.school_id = '.$school_id.' AND t.token = "'.$user_id.'" UNION SELECT n.* FROM notification n WHERE n.roles_id = "all" and n.school_id = '.$school_id);
        	log_message('debug',$this->db->last_query()); 
-        if($query)
+        if($query1)
         {
-            return $query->result();
+            return $query1->result();
         }
     } 
     
