976a18a443b9dee7bb71134f19c6c5980ac97e09
SharayuFaye <sharayu@servicenet.in>
2019-12-06 12:09:48 +0530

    remove order by

--- application/models/M_notifications.php
+++ application/models/M_notifications.php
@@ -151,8 +151,7 @@ class m_notifications extends CI_Model {
 		    
 		    $this->db->select("nt.* ")
 		    ->from("notification nt")
-		    ->where(array( 'nt.roles_id' =>  "all", 'n.school_id'=>$school_id))
-		    ->order_by('nt.datetime','desc');
+		    ->where(array( 'nt.roles_id' =>  "all", 'n.school_id'=>$school_id));
 		    
 		    //            $this->db->order_by('n.datetime','desc');
 		    $this->db->get();
@@ -161,13 +160,7 @@ class m_notifications extends CI_Model {
 		    $query =  $this->db->query($query1." UNION ".$query2);
 		    
 		    
-		}
-//         $this->db->order_by('n.datetime','desc');
-        
-       
-        
-        
-//         $query1 = $this->db->query('SELECT n.* FROM notification n JOIN users u ON n.roles_id = u.role LEFT JOIN tokens t ON t.user_id = u.id WHERE n.school_id = '.$school_id.' AND t.token = "'.$user_id.'" UNION SELECT n.* FROM notification n WHERE n.roles_id = "all" and n.school_id = '.$school_id);
+		} 
         log_message('debug',$this->db->last_query());
         if($query)
         {
